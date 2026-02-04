8d961d532aa0ced61174b6c7cc48f89a326bf1d4
oxocero <emielhardy@hotmail.com>
2026-02-04 04:09:12 +0100

    scaffolding

---
+++ include/winpatina.h
@@ -0,0 +1,20 @@
+/**
+ * @file winpatina.h
+ * @brief WinPatina - VT to Win32 Console translation layer
+ *
+ * WinPatina enables modern terminal applications that emit VT/ANSI escape
+ * sequences to run on legacy Windows systems (Windows 2000 through Windows 10
+ * pre-1511) that lack native Virtual Terminal Processing support.
+ *
+ * On modern terminals, WinPatina passes VT sequences through unchanged.
+ * On legacy systems, it translates them to Win32 Console API calls.
+ */
+
+#ifndef WINPATINA_H
+#define WINPATINA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#endif /* WINPATINA_H */
