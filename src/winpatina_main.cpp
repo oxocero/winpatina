a1ef10e0558f88c43c0bbbe5a60ad5305be024c7
oxocero <emielhardy@hotmail.com>
2026-02-05 01:20:17 +0100

    scaffolding

---
+++ src/winpatina_main.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file winpatina_main.cpp
+ * @brief Core WinPatina implementation
+ *
+ * Implements the main public API: initialisation, destruction, and queries.
+ */
+
+#include "internal.h"
+#include <cstdio>    /* snprintf */
+#include <cstdlib>   /* malloc, free */
+#include <cstring>   /* strncpy */
+
